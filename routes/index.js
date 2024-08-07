05dba0f72c03dce327c9bbbd5e65b3d9d5973a49
ADX119 <atherva15@gmail.com>
2024-08-08 01:19:37 +0530

    added express-session and flashconnect

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
-
+const isloggedin = require("../middlewares/isLoggedIn")
 const router = express.Router();
 
 router.get("/",function(req,res){
-    res.render("index");
+    let error = req.flash("error");
+    res.render("index",{error});
 });
+router.get("/shop",isloggedin,function(req,res){
+    res.render("shop");
+})
 
 
 
